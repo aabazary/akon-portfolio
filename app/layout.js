2d53eef341c125d1274cc4871bfed8dc892604e2
aabazary <85041715+aabazary@users.noreply.github.com>
2025-02-12 18:57:05 -0800

    updated title and favicon

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 export const metadata = {
-  title: "My Portfolio",
+  title: "Akon's Portfolio",
   description: "A portfolio showcasing my projects and skills",
 };
 
